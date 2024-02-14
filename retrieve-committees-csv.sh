ee5e4fe70d5bba2e81824db26c0a18e62bcffd1e
Hunter Fuller <hfuller@pixilic.com>
2024-02-14 00:04:52 -0600

    specify the body is senate (or whatever...)

--- retrieve-committees-csv.sh
+++ retrieve-committees-csv.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-gq https://gql.api.alison.legislature.state.al.us/graphql -H "Content-type: application/json" -q '{committees(direction:"asc"orderBy:"committee"limit:"15"offset:"0" customFilters: {}){ CommitteeId,Committee,CommitteeClerk,CommitteeContact,CommitteePhone,ContactLocation }}' | jq '.data.committees' | jq -r '(.[0] | keys_unsorted) as $keys | $keys, map([.[ $keys[] ]])[] | @csv' > committees.csv
+gq https://gql.api.alison.legislature.state.al.us/graphql -H "Content-type: application/json" -q '{committees(body: "Senate", direction:"asc"orderBy:"committee"limit:"150"offset:"0" customFilters: {}){ CommitteeId,Committee,CommitteeClerk,CommitteeContact,CommitteePhone,ContactLocation }}' | jq '.data.committees' | jq -r '(.[0] | keys_unsorted) as $keys | $keys, map([.[ $keys[] ]])[] | @csv' > committees.csv
